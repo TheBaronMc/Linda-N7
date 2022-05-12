de0033830a1a84248b3150e142a096d67e1a3c22
charly <charly@MacBook-Pro-de-charly-2.local>
2022-05-12 10:58:40 +0200

    Semaphore -> monitor

--- linda/shm/CentralizedLinda.java
+++ linda/shm/CentralizedLinda.java
@@ -7,176 +7,149 @@ import linda.Tuple;
 import java.util.*;
 import java.util.concurrent.Semaphore;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+
 /** Shared memory implementation of Linda. */
 public class CentralizedLinda implements Linda {
 
     private ArrayList<Tuple> tuplesList;
-    private HashMap<Tuple, Callback> readCallbacks;
-    private HashMap<Tuple, Callback> takeCallbacks;
+    private ArrayList<EventHandler> readEvents;
+    private ArrayList<EventHandler> takeEvents;
 
-    private int inReadQueue;
-    private Semaphore readQueue;
-    private int inTakeQueue;
-    private Semaphore takeQueue;
-
-    private Semaphore mutex;
+    private Lock mutex;
+    private Condition readCondition;
+    private Condition takeCondition;
 
     public CentralizedLinda() {
         this.tuplesList = new ArrayList<>();
-        this.readCallbacks = new HashMap<>();
-        this.takeCallbacks = new HashMap<>();
+        this.readEvents = new ArrayList<>();
+        this.takeEvents = new ArrayList<>();
 
-        this.inReadQueue = 0;
-        this.readQueue = new Semaphore(0);
-        this.inTakeQueue = 0;
-        this.takeQueue = new Semaphore(0);
-
-        this.mutex = new Semaphore(1);
+        this.mutex = new ReentrantLock();
+        this.readCondition = this.mutex.newCondition();
+        this.takeCondition = this.mutex.newCondition();
     }
 
     @Override
     public void write(Tuple t) {
-        try {
-            this.mutex.acquire();
+        this.mutex.lock();
 
-            // Read Callbacks
-            for(Map.Entry<Tuple, Callback> entry : this.readCallbacks.entrySet()) {
-                Tuple template = entry.getKey();
+        ArrayList<Tuple> found;
 
-                if (t.matches(template)) {
-                    Callback callback = entry.getValue();
-                    callback.call(new Tuple(t));
-                }
+        // Read Callbacks
+        for (EventHandler event : this.readEvents) {
+            if (event.worksWith(t)) {
+                event.callWith(t.deepclone());
+                this.readEvents.remove(event);
             }
-
-            Set<Tuple> takeTemplates = this.takeCallbacks.keySet();
-            for (Tuple template : takeTemplates) {
-                if (t.matches(template)) {
-                    Callback callback = this.takeCallbacks.get(template);
-                    callback.call(t);
-                    return;
-                }
-            }
-
-            this.tuplesList.add(t);
-
-            this.readQueue.release(this.inReadQueue);
-            this.inReadQueue = 0;
-            this.takeQueue.release(this.inTakeQueue);
-            this.inTakeQueue = 0;
-
-            this.mutex.release();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
+
+        // Take Callbacks
+        for (EventHandler event : takeEvents) {
+            if (event.worksWith(t)) {
+                event.callWith(t);
+                this.takeEvents.remove(event);
+                this.mutex.unlock();
+                return;
+            }
+        }
+
+        this.tuplesList.add(t);
+
+        this.readCondition.signalAll();
+        this.takeCondition.signalAll();
+
+        this.mutex.unlock();
     }
 
     @Override
     public Tuple take(Tuple template) {
-        try {
-            this.mutex.acquire();
+        this.mutex.lock();
 
-            while (!existTuple(template)) {
-                this.inTakeQueue++;
-                this.mutex.release();
-                this.takeQueue.acquire();
-                this.mutex.acquire();
+        while (!existTuple(template)) {
+            try {
+                this.takeCondition.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-
-            Tuple tuple = this.getTuple(template);
-
-            this.mutex.release();
-            return tuple;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
+
+        Tuple tuple = this.getTuple(template);
+
+        this.mutex.unlock();
+
+        return tuple;
     }
 
     @Override
     public Tuple read(Tuple template) {
-        try {
-            this.mutex.acquire();
+        this.mutex.lock();
 
-            while (!existTuple(template)) {
-                this.inReadQueue++;
-                this.mutex.release();
-                this.readQueue.acquire();
-                this.mutex.acquire();
+        while (!this.existTuple(template)) {
+            try {
+                this.readCondition.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-
-            Tuple tuple = this.copyTuple(template);
-
-            this.mutex.release();
-            return tuple;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
+
+        Tuple tuple = this.copyTuple(template);
+
+        this.mutex.unlock();
+
+        return tuple;
     }
 
     @Override
     public Tuple tryTake(Tuple template) {
-        try {
-            this.mutex.acquire();
-            Tuple tuple = this.getTuple(template);
-            this.mutex.release();
+        this.mutex.lock();
+        Tuple tuple = this.getTuple(template);
+        this.mutex.unlock();
 
-            return tuple;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        return tuple;
     }
 
     @Override
     public Tuple tryRead(Tuple template) {
-        try {
-            this.mutex.acquire();
-            Tuple tuple = this.copyTuple(template);
-            this.mutex.release();
+        this.mutex.lock();
+        Tuple tuple = this.copyTuple(template);
+        this.mutex.unlock();
 
-            return tuple;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        return tuple;
     }
 
     @Override
     public Collection<Tuple> takeAll(Tuple template) {
-        try {
-            ArrayList<Tuple> col = new ArrayList<>();
+        ArrayList<Tuple> col = new ArrayList<>();
 
-            this.mutex.acquire();
+        this.mutex.lock();
 
-            while (!this.existTuple(template)) {
-                col.add(this.getTuple(template));
-            }
-
-            this.mutex.release();
-
-            return col;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        while (this.existTuple(template)) {
+            col.add(this.getTuple(template));
         }
+
+        this.mutex.unlock();
+
+        return col;
     }
 
     @Override
     public Collection<Tuple> readAll(Tuple template) {
-        try {
-            ArrayList<Tuple> col = new ArrayList<>();
+        ArrayList<Tuple> col = new ArrayList<>();
 
-            this.mutex.acquire();
+        this.mutex.lock();
 
-            for (Tuple tuple : this.tuplesList) {
-                if (tuple.matches(template)) {
-                    col.add(new Tuple(tuple));
-                }
+        for (Tuple tuple : this.tuplesList) {
+            if (tuple.matches(template)) {
+                col.add(tuple.deepclone());
             }
-
-            this.mutex.release();
-
-            return col;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
+
+        this.mutex.unlock();
+
+        return col;
     }
 
     @Override
@@ -192,19 +165,15 @@ public class CentralizedLinda implements Linda {
         }
 
         if (tuple == null) {
-            try {
-                this.mutex.acquire();
+            this.mutex.lock();
 
-                switch (mode) {
-                    case READ -> this.readCallbacks.put(template, callback);
-                    case TAKE -> this.takeCallbacks.put(template, callback);
-                    default -> throw new RuntimeException("Unknow mode : " + mode);
-                }
-
-                this.mutex.release();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            switch (mode) {
+                case READ -> this.readEvents.add(new EventHandler(template, callback));
+                case TAKE -> this.takeEvents.add(new EventHandler(template, callback));
+                default -> throw new RuntimeException("Unknow mode : " + mode);
             }
+
+            this.mutex.unlock();
         } else {
             callback.call(tuple);
         }
@@ -216,8 +185,8 @@ public class CentralizedLinda implements Linda {
 
         System.out.println("------------- TUPLESPACE --------------");
 
-        for (int i = 0; i < this.tuplesList.size(); i++)
-            System.out.println(this.tuplesList.get(i));
+        for (Tuple tuple : this.tuplesList)
+            System.out.println(tuple);
 
 
         System.out.println("================= END =================");
@@ -245,10 +214,28 @@ public class CentralizedLinda implements Linda {
     private Tuple copyTuple(Tuple template) {
         for (Tuple tuple : this.tuplesList) {
             if (tuple.matches(template)) {
-                return new Tuple(tuple);
+                return tuple.deepclone();
             }
         }
         return null;
     }
 
+    private class EventHandler {
+
+        private Tuple template;
+        private Callback callback;
+
+        public EventHandler(Tuple template, Callback callback) {
+            this.template = template;
+            this.callback = callback;
+        }
+
+        public boolean worksWith(Tuple tuple) {
+            return this.template.contains(tuple);
+        }
+
+        public void callWith(Tuple tuple) {
+            this.callback.call(tuple);
+        }
+    }
 }
--- linda/test/TestCentrelizedLinda.java
+++ linda/test/TestCentrelizedLinda.java
@@ -1,2 +1,135 @@
-package linda.test;public class TestCentrelizedLinda {
+package linda.test;
+
+import linda.Tuple;
+import linda.shm.CentralizedLinda;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCentrelizedLinda {
+
+    private CentralizedLinda linda;
+
+    @BeforeEach
+    void initTest() {
+        linda = new CentralizedLinda();
+    }
+
+    @Test
+    void TestTryTakeEmpty() {
+        Tuple motif = new Tuple(int.class, int.class);
+        Tuple tuple = linda.tryTake(motif);
+        assertNull(tuple);
+    }
+
+    @Test
+    void TestTryTake() {
+        Tuple t1 = new Tuple(1, 2);
+        Tuple t2 = new Tuple(3, 4);
+
+        linda.write(t1);
+        linda.write(t2);
+
+        Tuple motif = new Tuple(Integer.class, Integer.class);
+
+        Tuple tuple = linda.tryTake(motif);
+        assertNotNull(tuple);
+        assertTrue(t1 == tuple || t2 == tuple);
+
+        tuple = linda.tryTake(motif);
+        assertNotNull(tuple);
+        assertTrue(t1 == tuple || t2 == tuple);
+
+        tuple = linda.tryTake(motif);
+        assertNull(tuple);
+    }
+
+    @Test
+    void TestTryReadEmpty() {
+        Tuple motif = new Tuple(Integer.class, Integer.class);
+        Tuple tuple = linda.tryRead(motif);
+        assertNull(tuple);
+    }
+
+    @Test
+    void TestTryRead() {
+        Tuple t1 = new Tuple(1, 2);
+        Tuple t2 = new Tuple(3, 4);
+
+        linda.write(t1);
+        linda.write(t2);
+
+        Tuple motif = new Tuple(Integer.class, Integer.class);
+
+        Tuple tuple = linda.tryRead(motif);
+        assertNotNull(tuple);
+        assertTrue(t1 != tuple && t2 != tuple);
+        assertTrue(motif.contains(tuple));
+    }
+
+    @Test
+    void TestTakeAllEmpty() {
+        Tuple motif = new Tuple(Integer.class, Integer.class);
+
+        Collection<Tuple> tuples = linda.takeAll(motif);
+
+        assertEquals(0, tuples.size());
+    }
+
+    @Test
+    void TestTakeAll() {
+        Tuple t1 = new Tuple(1, 2);
+        Tuple t2 = new Tuple(3, 4);
+
+        linda.write(t1);
+        linda.write(t2);
+
+        Tuple motif = new Tuple(Integer.class, Integer.class);
+
+        Collection<Tuple> tuples;
+
+        tuples = linda.takeAll(motif);
+        assertEquals(2, tuples.size());
+        for (Tuple tuple : tuples)
+            assertTrue(motif.contains(tuple));
+
+        tuples = linda.takeAll(motif);
+        assertEquals(0, tuples.size());
+    }
+
+    @Test
+    void TestReadAllEmpty() {
+        Tuple motif = new Tuple(Integer.class, Integer.class);
+
+        Collection<Tuple> tuples = linda.readAll(motif);
+
+        assertEquals(0, tuples.size());
+    }
+
+    @Test
+    void TestReadAll() {
+        Tuple t1 = new Tuple(1, 2);
+        Tuple t2 = new Tuple(3, 4);
+
+        linda.write(t1);
+        linda.write(t2);
+
+        Tuple motif = new Tuple(Integer.class, Integer.class);
+
+        Collection<Tuple> tuples;
+
+        tuples = linda.readAll(motif);
+        assertEquals(2, tuples.size());
+        for (Tuple tuple : tuples) {
+            assertTrue(tuple != t1 && tuple != t2);
+            assertTrue(motif.contains(tuple));
+        }
+
+        tuples = linda.takeAll(motif);
+        assertNotEquals(0, tuples.size());
+    }
 }
